2b3d030ec6e231e813c6d96abb21bd12fc2617f2
cooc <minhdaonguyenad@gmail.com>
2026-09-23 22:41:29 +0700

    Open control panel before checking exam view in emulator test

--- integration_test/app_flow_test.dart
+++ integration_test/app_flow_test.dart
@@ -1,5 +1,6 @@
 import 'package:better_phenikaa_schedule/app/app.dart';
 import 'package:better_phenikaa_schedule/features/dang_nhap_qldt/qldt_models.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -56,7 +57,9 @@ void main() {
     await tester.tap(find.text('Theo ngày'));
     await tester.pumpAndSettle();
     expect(find.text('Thiết kế web nâng cao'), findsWidgets);
-    await tester.tap(find.text('Lịch thi').last);
+    await tester.tap(find.byType(FloatingActionButton));
+    await tester.pumpAndSettle();
+    await tester.tap(find.text('Lịch thi'));
     await tester.pumpAndSettle();
     expect(find.text('Thiết kế web nâng cao'), findsWidgets);
   });
